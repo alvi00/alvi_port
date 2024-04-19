4b32838ff820cc1b7aae96d84a186bd3fe1437b2
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-19 12:50:18 +0600

    Add files via upload

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'ahmadfahmid59@gmail.com',
+    phone: '+8801401083338',
+    address: 'Dhaka, Bangladesh',
+    github: 'https://github.com/alvi00',
+    facebook: 'https://www.facebook.com/ahmadfahmid.alvi.12/',
+    linkedIn: 'https://www.linkedin.com/in/ahmad-fahmid/',
+    twitter: 'https://twitter.com/CAPTAIN35857918',
+    stackOverflow: 'https://stackoverflow.com/users/24472673/ahmad-fahmid',
+    devUsername: "alvi007"
 }
