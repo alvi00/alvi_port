aae5a01743ece280e80b2a9e6d8485ba73cb7848
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-24 14:42:34 +0600

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -5,60 +5,40 @@ import travel from '/public/image/travel.jpg';
 
 export const projectsData = [
     {
-        id: 1,
-        name: 'Car_Buying_Software',
-        description: "This Project is basically for my academic purpose. Here I have made a Car buying Website and tried to make it quite similar to the Porsche Website. Here I have used java,JavaFX,CSS,JavaMail API for this project. Thanks a lot, This is pre- release or beta version. We are having some problems with fonts and car specs and some heap space-related issues with the runnable jar file. For the code, it runs pretty smooth, but a heap storage-related problem is there in the code. Feel free to contribute.",
-        tools: ['Java', 'Javfx', 'JavaMail API', 'Css'],
-        Link: 'https://github.com/alvi00/Car_Buying_Website_Project_In_Java-_javafx-_css-_javamail_API',
-        code: '',
-        demo: '',
-        image: crefin,
+      id: 1,
+      name: 'Car_Buying_Software',
+      description: "This Project is basically for my academic purpose. Here I have made a Car buying Website and tried to make it quite similar to the Porsche Website. Here I have used java,JavaFX,CSS,JavaMail API for this project. Thanks a lot, This is pre- release or beta version. We are having some problems with fonts and car specs and some heap space-related issues with the runnable jar file. For the code, it runs pretty smooth, but a heap storage-related problem is there in the code. Feel free to contribute.",
+      tools: ['Java', 'Javfx', 'JavaMail API', 'Css'],
+      githubLink: 'https://github.com/alvi00/Car_Buying_Website_Project_In_Java-_javafx-_css-_javamail_API',
+      websiteLink: '', // Add website link here
+      image: crefin,
     },
     {
-        id: 2,
-        name: 'Epic Games Website Clone',
-        description: 'I tried to make a clone of Epic Games Website. I only did the fronted clone. Visit - ,  Github Link - https://github.com/alvi00/Epic-Games-Clone',
-        tools: ['Html', 'Bootsrap CSS', "CSS", "Javascript"],
-        Link: 'https://alvi00.github.io/Epic-Games-Clone/',
-        code: '',
-        demo: '',
-        image: travel,
+      id: 2,
+      name: 'Epic Games Website Clone',
+      description: 'I tried to make a clone of Epic Games Website. I only did the fronted clone. Visit - ,  Github Link - https://github.com/alvi00/Epic-Games-Clone',
+      tools: ['Html', 'Bootsrap CSS', "CSS", "Javascript"],
+      githubLink: 'https://github.com/alvi00/Epic-Games-Clone',
+      websiteLink: 'https://alvi00.github.io/Epic-Games-Clone/',
+      image: travel,
     },
     {
-        id: 3,
-        name: 'Play-Bytes-Resturant-enhanced-with-AI',
-        description: 'This is a project by nsu_cenace.. The purpose was to create a restaurant management system in which we can have discount features and add games to the website. Here is a game named cenace which is made with AI. Me along with my two friends fahim and saad made this project together',
-        tools: ['C'],
-        code: '',
-        Link: 'https://github.com/alvi00/Play-Bytes-Resturant-enhanced-with-AI',
-        demo: '',
-        image: realEstate,
+      id: 3,
+      name: 'Play-Bytes-Resturant-enhanced-with-AI',
+      description: 'This is a project by nsu_cenace.. The purpose was to create a restaurant management system in which we can have discount features and add games to the website. Here is a game named cenace which is made with AI. Me along with my two friends fahim and saad made this project together',
+      tools: ['C'],
+      githubLink: 'https://github.com/alvi00/Play-Bytes-Resturant-enhanced-with-AI',
+      websiteLink: '', // Add website link here
+      image: realEstate,
     },
     {
-        id: 4,
-        name: 'Gorom Lagse Weather Website',
-        description: 'Website Link - https://gorom-porse.vercel.app Apk link- below. Introducing Gorom Porse - your new weather buddy! 🚀 Made this Using simple HTML, CSS, and JavaScript.With Gorom Porse, you get: 🌡️ Temperature updates, 📌 Todays Highlights 🌬️ Air Quality Index,🌅 Sunset and Sunrise times, 💧 Humidity levels,🌫️ Visibility...and more! Just click to see the weather in your area or explore forecasts for other countries. Easy peasy! 🌍 Website Link - https://gorom-porse.vercel.app Apk Link - https://drive.google.com/.../1KA5HsTIZGWhFLq2D1VKDmyuIbJi... GitHub Link - https://github.com/alvi00/gorom-porse feel free to contribute. Lets stay connected through the weather! ☔☀️',
-        tools: ['Css','Html','Javascript'],
-        code: '',
-        Link: 'https://gorom-porse.vercel.app/',
-        demo: '',
-        image: realEstate,
+      id: 4,
+      name: 'Gorom Lagse Weather Website',
+      description: 'Website Link - https://gorom-porse.vercel.app Apk link- below. Introducing Gorom Porse - your new weather buddy! 🚀 Made this Using simple HTML, CSS, and JavaScript.With Gorom Porse, you get: 🌡️ Temperature updates, 📌 Todays Highlights 🌬️ Air Quality Index,🌅 Sunset and Sunrise times, 💧 Humidity levels,🌫️ Visibility...and more! Just click to see the weather in your area or explore forecasts for other countries. Easy peasy! 🌍 Website Link - https://gorom-porse.vercel.app Apk Link - https://drive.google.com/.../1KA5HsTIZGWhFLq2D1VKDmyuIbJi... GitHub Link - https://github.com/alvi00/gorom-porse feel free to contribute. Lets stay connected through the weather! ☔☀️',
+      tools: ['Css','Html','Javascript'],
+      githubLink: 'https://github.com/alvi00/gorom-porse',
+      websiteLink: 'https://gorom-porse.vercel.app/',
+      image: realEstate,
     }
-
-];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
-
+  ];
+  
